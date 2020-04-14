a54efc2bb186e4019bec498d2cbd108d9630ad2e
ethArek <arkadiusz.sidzinka@gmail.com>
2020-04-14 20:06:39 +0200

    feat: add openzepellin contracts and import erc20 interface

--- src/contracts/swap.sol
+++ src/contracts/swap.sol
@@ -1,19 +1,16 @@
 pragma solidity >=0.4.16 <0.7.0;
 
+import "@openzeppelin/token/ERC20/IERC20.sol";
+
 
 contract Swap {
     address owner;
     address constant opacityAddress = address(
         0x77599D2C6DB170224243e255e6669280F11F1473
     );
+    IERC20 constant opacityToken = IERC20(opacityAddress);
     uint8[] validTokenAmounts = [2, 16, 32];
     uint256 constant tokenPrice = 75000000000000;
-
-    struct SwapTransaction {
-        uint256 tokenAmount;
-        uint256 price;
-        address buyer;
-    }
 
     modifier ownerOnly {
         require(msg.sender == owner, "You are not the owner of contract");
@@ -34,5 +31,7 @@ contract Swap {
             msg.value >= amount * tokenPrice,
             "You sent incorrect amount of Ether"
         );
+
+        opacityToken.transfer(msg.sender, amount);
     }
 }
