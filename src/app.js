ca039b089cc3420825e50109bd4aa2128425c02b
ethArek <arkadiusz.sidzinka@gmail.com>
2020-04-15 19:57:20 +0200

    feat: remove handlebars enngine

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
-const exphbs = require('express-handlebars');
 
 const PORT = 3000;
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
 app.get('/', (req, res) => {
-  res.render('home');
+  res.send('home');
 });
 
 app.listen(PORT, () => {
